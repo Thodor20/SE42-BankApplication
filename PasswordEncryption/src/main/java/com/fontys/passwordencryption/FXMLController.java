6d9dc5b9268465395ed5763f823d1723c694f967
i340824 <s.delaat@student.fontys.nl>
2017-06-13 11:57:23 +0200

    password encrypt

---
+++ PasswordEncryption/src/main/java/com/fontys/passwordencryption/FXMLController.java
@@ -0,0 +1,43 @@
+package com.fontys.passwordencryption;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class FXMLController implements Initializable {
+    
+    @FXML
+    private Label laSecret;
+    
+    @FXML
+    private Button btnEncrypt;
+    
+    @FXML
+    private Button btnDecrypt;
+    
+    @FXML
+    private TextField tfMessage;
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+    
+    @FXML
+    private void encryptMessage_Click(){
+        
+    }
+    
+    @FXML
+    private void decryptMessage_Click(){
+        
+    }
+    
+    private void encryptMessage(){
+        
+    }
+}
