78bbdbc05a9bc8fa1a50d9ec5793706a45e26b8f
i340824 <s.delaat@student.fontys.nl>
2017-06-13 15:22:04 +0200

    File controller

--- PasswordEncryption/src/main/java/com/fontys/passwordencryption/FileControl.java
+++ PasswordEncryption/src/main/java/com/fontys/passwordencryption/FileControl.java
@@ -6,9 +6,11 @@
 package com.fontys.passwordencryption;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -16,11 +18,39 @@ import java.util.logging.Level;
  */
 public class FileControl {
     
-    public void writeFile(File file, byte [] data){
-         try (FileOutputStream fos = new FileOutputStream(file)) {
+    private static final Logger LOGGER = Logger.getLogger(FileControl.class.getName());
+
+    /**
+     *
+     * @param filename
+     * @param data
+     * @return
+     */
+    public boolean writeData(String filename, byte[] data) {
+        try (FileOutputStream fos = new FileOutputStream(filename)) {
             fos.write(data);
+            return true;
         } catch (IOException ex) {
-            
+            LOGGER.log(Level.SEVERE, null, ex);
+            return false;
+        }
+    }
+
+    /**
+     *
+     * @param file
+     * @return
+     */
+    public byte[] readData(File file) {
+        
+
+        try (FileInputStream fis = new FileInputStream(file)) {
+            byte[] keyBytes = new byte[(int) file.length()];
+            fis.read(keyBytes);
+            return keyBytes;
+        } catch (IOException ex) {
+            LOGGER.log(Level.SEVERE, null, ex);
+            return null;
         }
     }
 }
